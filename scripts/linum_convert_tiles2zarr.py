9b8625d28b7cc3c059d7412e484ed89e00bd3b26
Joël Lefebvre <lefebvre.joel@uqam.ca>
2024-02-08 10:39:44 -0500

    Script to convert a tile into a ome-zarr for visualization

---
+++ scripts/linum_convert_tiles2zarr.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Convert 3D OCT tiles to zarr"""
+
+from pathlib import Path
+
+import dask.array as da
+import numpy as np
+import zarr
+from tqdm.auto import tqdm
+import shutil
+
+from linumpy import reconstruction
+from linumpy.io.zarr import save_zarr
+from linumpy.microscope.oct import OCT
+
+# Tasks
+# TODO: flag to choose between zarr for reconstruction (small chunks), or for visualization (smaller chunks)
+# TODO: use dask and data loader to reduce IO and reduce memory usage.
+# TODO: add option to keep all the scan or crop the galvo return (this assumes that the galvo return is in the right position)
+# TODO: add option to choose the smallest resolution (downsample) instead of using the maximum resolution
+# TODO: add option to crop
+
+# Parameters
+tiles_directory = Path("/Users/jlefebvre/Downloads/tiles_z25_for_Joel")
+tmp_zarr_directory = Path("/Users/jlefebvre/Downloads/tiles_z25_for_Joel.zarr")
+output_zarr = Path("/Users/jlefebvre/Downloads/tiles_z25_for_Joel.ome_zarr")
+z = 25
+n_rot = 3
+flip_lr = False
+flip_ud = False
+crop = False
+n_levels = 5
+
+
+def preprocess_volume(oct) -> np.ndarray:
+    vol = oct.load_image(crop=False)
+    vol = np.rot90(vol, k=3, axes=(1, 2))
+    vol = np.flip(vol, axis=1)
+    return vol
+
+# Prepare the zarr information
+chunk_size = 128
+chunks = tuple([chunk_size] * 3)
+
+# Analyze the tiles
+tiles, tiles_pos = reconstruction.get_tiles_ids(tiles_directory, z=z)
+mx = [tiles_pos[i][0] for i in range(len(tiles_pos))]
+my = [tiles_pos[i][1] for i in range(len(tiles_pos))]
+mz = [tiles_pos[i][2] for i in range(len(tiles_pos))]
+mx_min = min(mx)
+mx_max = max(mx)
+my_min = min(my)
+my_max = max(my)
+mz_min = min(mz)
+mz_max = max(mz)
+n_mx = mx_max - mx_min + 1
+n_my = my_max - my_min + 1
+n_mz = mz_max - mz_min + 1
+
+# Prepare the mosaic_grid and chunk size
+oct = OCT(tiles[0])
+vol = preprocess_volume(oct)
+resolution = [oct.resolution[2], oct.resolution[0], oct.resolution[1]]
+mosaic_shape = [vol.shape[0], n_mx * vol.shape[1], n_my * vol.shape[2]]
+chunk_size = [vol.shape[2], vol.shape[0], vol.shape[1]]
+
+# Assemble an initial zarr
+store = zarr.NestedDirectoryStore(tmp_zarr_directory)
+root = zarr.group(store=store, overwrite=True)
+ome_root = zarr.group(store=zarr.NestedDirectoryStore(tmp_zarr_directory), overwrite=True)
+
+# Add a group
+mosaic = root.zeros(f"slice_{z:02d}", shape=mosaic_shape, chunks=oct.shape, dtype=np.float32)
+
+for i in tqdm(range(len(tiles)), desc="Reading tiles"):
+    f = tiles[i]
+    mx, my, mz = tiles_pos[i]
+    oct = OCT(f)
+    vol = preprocess_volume(oct)
+    rmin = mx * vol.shape[1]
+    cmin = my * vol.shape[2]
+    rmax = rmin + vol.shape[1]
+    cmax = cmin + vol.shape[2]
+    mosaic[:, rmin:rmax, cmin:cmax] = vol
+
+# Convert to ome-zarr
+mosaic_dask = da.from_zarr(mosaic)
+save_zarr(mosaic_dask, output_zarr, scales=resolution, chunks=chunks, n_levels=n_levels)
+
+# Remove the temporary zarr
+shutil.rmtree(tmp_zarr_directory)
