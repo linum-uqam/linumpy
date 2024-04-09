ab699e04707e26c58c84020d22cf94dbe1926a04
Joël Lefebvre <lefebvre.joel@uqam.ca>
2024-04-09 14:39:06 -0400

    Adapting the script to run in parallel with multiple processors

--- scripts/linum_fix_illumination_3d.py
+++ scripts/linum_fix_illumination_3d.py
@@ -3,17 +3,25 @@
 
 """Detect and fix the lateral illumination inhomogeneities for each 3D tiles of a mosaic grid"""
 
+from os import environ
+
+environ["OMP_NUM_THREADS"] = "1"
+
 import argparse
+import multiprocessing
 import shutil
+import tempfile
 from pathlib import Path
 
 import zarr
 from pybasic.shading_correction import BaSiC
 from tqdm.auto import tqdm
+import imageio as io
+import numpy as np
+from pqdm.processes import pqdm
 
 
 # TODO: add option to export the flatfields and darkfields
-# TODO: parallelize the processing of the tiles
 
 def _build_arg_parser():
     p = argparse.ArgumentParser(
@@ -28,24 +36,25 @@ def _build_arg_parser():
 
 def process_tile(params: dict):
     """Process a tile and add it to the output mosaic"""
-    vol = params["vol"]
-    vol_output = params["vol_output"]
+    file = params["slice_file"]
     z = params["z"]
+    tile_shape = params["tile_shape"]
+    vol = io.v3.imread(str(file))
+    file_output = Path(file).parent / file.name.replace(".tiff", "_corrected.tiff")
 
     # Get the number of tiles
-    tile_shape = vol.chunks
-    nx = vol.shape[1] // tile_shape[1]
-    ny = vol.shape[2] // tile_shape[2]
+    nx = vol.shape[0] // tile_shape[0]
+    ny = vol.shape[1] // tile_shape[1]
 
     # Extract the tiles for this slice
     tiles = []
     for i in range(nx):
         for j in range(ny):
-            rmin = i * tile_shape[1]
-            rmax = (i + 1) * tile_shape[1]
-            cmin = j * tile_shape[2]
-            cmax = (j + 1) * tile_shape[2]
-            tiles.append(vol[z, rmin:rmax, cmin:cmax])
+            rmin = i * tile_shape[0]
+            rmax = (i + 1) * tile_shape[0]
+            cmin = j * tile_shape[1]
+            cmax = (j + 1) * tile_shape[1]
+            tiles.append(vol[rmin:rmax, cmin:cmax])
 
     # Estimate the illumination bias
     optimizer = BaSiC(tiles, estimate_darkfield=True)
@@ -60,13 +69,18 @@ def process_tile(params: dict):
         tiles_corrected.append(optimizer.normalize(tile))
 
     # Fill the output mosaic
+    vol_output = np.zeros_like(vol)
     for i in range(nx):
         for j in range(ny):
-            rmin = i * tile_shape[1]
-            rmax = (i + 1) * tile_shape[1]
-            cmin = j * tile_shape[2]
-            cmax = (j + 1) * tile_shape[2]
-            vol_output[z, rmin:rmax, cmin:cmax] = tiles_corrected[i * ny + j]
+            rmin = i * tile_shape[0]
+            rmax = (i + 1) * tile_shape[0]
+            cmin = j * tile_shape[1]
+            cmax = (j + 1) * tile_shape[1]
+            vol_output[rmin:rmax, cmin:cmax] = tiles_corrected[i * ny + j]
+
+    io.imsave(str(file_output), vol_output)
+
+    return z, file_output
 
 
 def main():
@@ -77,26 +91,41 @@ def main():
     # Parameters
     input_zarr = Path(args.input_zarr)
     output_zarr = Path(args.output_zarr)
+    n_cpus = multiprocessing.cpu_count() - 1
 
+    # Prepare the data for the parallel processing
     vol = zarr.open(input_zarr, mode="r")
+    n_slices = vol.shape[0]
+    tmp_dir = tempfile.TemporaryDirectory(suffix="_linum_fix_illumination_3d_slices", dir=output_zarr.parent)
+    params_list = []
+    for z in tqdm(range(n_slices), "Preprocessing slices"):
+        slice_file = Path(tmp_dir.name) / f"slice_{z:03d}.tiff"
+        img = vol[z]
+        io.imsave(str(slice_file), img)
+        params = {
+            "z": z,
+            "slice_file": slice_file,
+            "tile_shape": vol.chunks[1:],
+        }
+        params_list.append(params)
 
-    # Prepare the output zarr
+    # Process the tiles in parallel
+    corrected_files = pqdm(params_list, process_tile, n_jobs=n_cpus, desc="Processing tiles")
+
+    # Retrieve the results and fix the volume
     process_sync_file = str(output_zarr).replace(".zarr", ".sync")
     synchronizer = zarr.ProcessSynchronizer(process_sync_file)
     vol_output = zarr.open(output_zarr, mode="w", shape=vol.shape, dtype=vol.dtype, chunks=vol.chunks,
                            synchronizer=synchronizer)
-
-    # Prepare the data for each process
-    for z in tqdm(range(vol.shape[0])):
-        params = {
-            "z": z,
-            "vol": vol,
-            "vol_output": vol_output,
-        }
-        process_tile(params)
+    for z, f in tqdm(corrected_files, "Rebuilding volume"):
+        slice_vol = io.v3.imread(str(f))
+        vol_output[z] = slice_vol[:]
 
     # Remove the process sync file
     shutil.rmtree(process_sync_file)
+
+    # Remove the temporary slice files used by the parallel processes
+    tmp_dir.cleanup()
 
 
 if __name__ == "__main__":
