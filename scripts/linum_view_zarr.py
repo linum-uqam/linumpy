ad50c799918a85ddb0487dab2d56787ce6a12d50
Joël Lefebvre <lefebvre.joel@uqam.ca>
2024-07-24 17:07:53 -0400

    Updating the documentation

--- scripts/linum_view_zarr.py
+++ scripts/linum_view_zarr.py
@@ -14,8 +14,8 @@ def _build_arg_parser():
         description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
     p.add_argument("input_zarr",
                    help="Full path to the Zarr file.")
-    p.add_argument("-r", "--resolution",  nargs="+", type=float, default=1.0,
-                   help="Resolution in micrometer (default=%(default)s)")
+    p.add_argument("-r", "--resolution",  nargs="+", type=float, default=[1.0],
+                   help="Resolution in micrometer in the Z, X, Y order. For an isotropic resolution, provide a single value. (default=%(default)s)")
 
     return p
 
