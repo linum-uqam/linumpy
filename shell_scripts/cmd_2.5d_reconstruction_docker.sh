25630cd75799c47cc896022e92fc8ae350c65752
Joël Lefebvre <lefebvre.joel@uqam.ca>
2024-08-02 11:23:32 -0400

    Removing a dependence

--- shell_scripts/cmd_2.5d_reconstruction_docker.sh
+++ shell_scripts/cmd_2.5d_reconstruction_docker.sh
@@ -3,7 +3,6 @@
 # Parameters
 DIRECTORY=/Users/jlefebvre/Downloads/2024-05-16-Mounier-15-Horizontal
 WORKFLOW_FILE=$DIRECTORY/workflow_reconstruction_2.5d.nf
-CONFIG_FILE=$DIRECTORY/reconstruction_2.5d_macbook_joel.config
-SINGULARITY=$DIRECTORY/linumpy.sif
+CONFIG_FILE=$DIRECTORY/reconstruction_2.5d_macbook.config
 
 nextflow run $WORKFLOW_FILE -c $CONFIG_FILE --directory $DIRECTORY -resume
