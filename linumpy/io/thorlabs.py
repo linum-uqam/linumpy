164953b179c86ffb7a8717cbaf300b8f2a39a88c
MohamadHawshar <mohamad_hawchar@hotmail.com>
2024-11-10 14:19:37 -0500

    Added the ability to save as a complex zarr, and complex to float casting (Saving as zarr does not opperate as intended when using a custom resolution < --resolution 10> due to the use of skimage to resize, which discards the imaginary values

--- linumpy/io/thorlabs.py
+++ linumpy/io/thorlabs.py
@@ -3,6 +3,7 @@ from xml.dom.minidom import parse
 import numpy as np
 from imageio import volwrite
 import gc  # For garbage collection
+from pathlib import Path
 
 """This file contains functions for working with Thorlabs files"""
 
@@ -16,17 +17,17 @@ class ThorOCT:
         self.size_y= None
         self.size_z = None
         self.header = None
+        self.resolution = []
         
-    def load(self, erase_raw_data = True, erase_polarization_1 = False, erase_polarization_2 = False):
+    def load(self, erase_raw_data = True, erase_polarization_1 = False, erase_polarization_2 = False, return_complex = False):
         if not self.compressed_data:
             raise ValueError("No valid data source provided.")
         self._extract_oct_header()
         self._extract_complex_dimensions()
-        self._load_polarized_data(erase_polarization_1, erase_polarization_2)
+        self._load_polarized_data(erase_polarization_1, erase_polarization_2, return_complex)
         
         # If requested, erase the raw data source (compressed_data) itself
         if erase_raw_data:
-            print("Erasing the compressed raw data to free up memory.")
             self.compressed_data = None
             gc.collect()  # Force garbage collection
     
@@ -59,16 +60,22 @@ class ThorOCT:
             # Check for specific file paths
             if file_content == "data\\Complex.data":
                 complex_data_file = data_file
-                print(f"Found Complex Data File: {file_content}")
 
         # Optionally, access attributes of the found elements
         if complex_data_file:
             self.size_z = int(complex_data_file.getAttribute('SizeZ'))
             self.size_x = int(complex_data_file.getAttribute('SizeX'))
             self.size_y = int(complex_data_file.getAttribute('SizeY'))
-            print(f"Complex Data - SizeZ: {self.size_z}, SizeX: {self.size_x}, SizeY: {self.size_y}")
+            range_z = float(complex_data_file.getAttribute('RangeZ'))
+            range_y = float(complex_data_file.getAttribute('RangeY'))
+            range_x = float(complex_data_file.getAttribute('RangeX'))
+            self.resolution = [
+                range_x / self.size_x,
+                range_y / self.size_y,
+                range_z / self.size_z
+            ]
 
-    def _load_polarized_data(self, erase_polarization_1, erase_polarization_2) -> None:
+    def _load_polarized_data(self, erase_polarization_1, erase_polarization_2, return_complex) -> None:
         """
         Loads the polarization data from the zip file.
         """
@@ -78,19 +85,120 @@ class ThorOCT:
             raw_data2_file = "data/Complex_Cam1.data"
             # Load the data for polarization 1 and 2
             if not erase_polarization_1:
-                self.polarization1 = self._convert_to_numpy(file = raw_data1_file)
+                self.polarization1 = self._convert_to_numpy(file = raw_data1_file, return_complex = return_complex)
             if not erase_polarization_2:
-                self.polarization2 = self._convert_to_numpy(file = raw_data2_file)
+                self.polarization2 = self._convert_to_numpy(file = raw_data2_file, return_complex = return_complex)
         except KeyError as e:
-            print(f"Error loading data: {e}")
+            raise Exception(f"Error loading polarization data: {e}")
 
-    def _convert_to_numpy(self, file) -> np.ndarray:
+    def _convert_to_numpy(self, file, return_complex = False) -> np.ndarray:
         with self.compressed_data.open(file) as f:
-            return np.frombuffer(f.read(), dtype=np.complex64).reshape((self.size_x, self.size_y, self.size_z), order='C')
+            complex_data = np.frombuffer(f.read(), dtype=np.complex64).reshape((self.size_x, self.size_y, self.size_z), order='C')
+            if return_complex:
+                return complex_data
+            # Return the magnitude of complexe by default
+            return np.abs(complex_data).astype(np.float32)
+        
+    @staticmethod
+    def extract_positions_from_scan(scan_file_path: str):
+        """
+        Extracts the x, y positions from the .scan file.
+
+        Parameters:
+        - scan_file_path: Path to the .scan file.
+
+        Returns:
+        - list containing the x, y index positions.
+        - list containing the raw (during acquisition) x, y positions.
+        """
+        raw_positions = []
+        position_indexes = []
+        
+        with open(scan_file_path, 'r') as file:
+            lines = file.readlines()
+
+            # Find the start of the positions section
+            positions_section = False
+            for line in lines:
+                line = line.strip()
+
+                # Mark the start of the positions section
+                if line == "------Positions------":
+                    positions_section = True
+                    continue
+
+                # If in the positions section, extract x, y values
+                if positions_section:
+                    if line:  # Ignore empty lines
+                        # Split by comma and convert to float
+                        x, y = map(float, line.split(','))
+                        raw_positions.append((x, y, 0))
+        
+        # hold the index locations for the tile placements in the Zarr file. The PSOCT acquisition (in its current state) is traversing from top right to bottom left.               
+        for x in range(3, -1, -1): #range(4): 
+            for y in range(4):
+                position_indexes.append((x, y, 0))
+        return position_indexes, raw_positions
+    
+    def get_PSOCT_tiles_ids(tiles_directory: str, number_of_angles:int = 2):
+        """
+        Get the .scan file and all .oct files from the tiles_directory.
+
+        Parameters:
+        - tiles_directory: Path to the directory containing the OCT tiles.
+        - number_of_angles: Number of acquisition angles.
+
+        Returns:
+        - positions: positions of the tiles in 3d
+        - grouped_files: list of file paths ordered by angles.
+        """
+        # Convert the tiles_directory to a Path object
+        tiles_path = Path(tiles_directory)
+        
+        if not tiles_path.is_dir():
+            raise ValueError(f"Provided path '{tiles_directory}' is not a valid directory.")
+        
+        # Initialize variables to store the results
+        scan_file = None
+        oct_files = []
+        grouped_files = [[] for _ in range(number_of_angles)]
+
+        # Iterate through files in the directory
+        for file in tiles_path.iterdir():
+            # Check for .scan file
+            if file.suffix == ".scan":
+                scan_file = file
+                positions, _ = ThorOCT.extract_positions_from_scan(scan_file)
+            # Collect .oct files
+            elif file.suffix == ".oct":
+                oct_files.append(file)
+        
+        # If no .scan file is found, raise a warning
+        if scan_file is None:
+            raise ValueError("Warning: No .scan file found in the directory.")
+        
+        # If no .oct files are found, raise a warning
+        if not oct_files:
+            raise ValueError("Warning: No .oct files found in the directory.")
             
-# input_file = "../../Data/2024_07_25_mouse_CB_1slice_4anglesliceIdx1_0001_ModePolarization3D.oct"
-# oct = ThorOCT(path = input_file)
-# oct.load()
-# pol1 = oct.polarization2
-# tiff_file = input_file.replace(".oct", "_polarization2.tiff")
-# volwrite(tiff_file, np.abs(pol1))
+        for i, oct_file in enumerate(oct_files):
+            angle_index = i % number_of_angles  # Determine the angle based on file index
+            grouped_files[angle_index].append(oct_file)
+        print(f"File Count for Angle index = {angle_index + 1}: {len(grouped_files[angle_index])}")
+        print("Processing the following Files:")
+        for file in grouped_files[0]:
+            print(f"  - {file}")
+        return grouped_files, positions
+    
+    def preprocess_volume_PSOCT(vol: np.ndarray) -> np.ndarray:
+        """Preprocess the volume by rotating and flipping it."""
+        vol = vol.transpose(2, 0, 1)
+        return vol
+
+            
+input_file = "C:\\Users\\Mohamad Hawchar\\Concordia University - Canada\\NeuralABC as-psOCT Samples - data\\2024_07_25_mouse_CB_1slice_2anglesliceIdx1_SUCCESS\\2024_07_25_mouse_CB_1slice_4anglesliceIdx1_0017_ModePolarization3D.oct"
+oct = ThorOCT(path = input_file)
+oct.load()
+pol1 = oct.polarization2
+tiff_file = "C:\\Users\\Mohamad Hawchar\\School\\UQAM\\LINUM\\PS-OCT\\m17_polarization2.tiff"
+volwrite(tiff_file, np.abs(pol1))
--- scripts/linum_create_mosaic_grid_3d.py
+++ scripts/linum_create_mosaic_grid_3d.py
@@ -37,6 +37,8 @@ def _build_arg_parser():
                    help="Polarization index to process (In case of PSOCT data type)")
     p.add_argument('--angle_index', type = int, default = 0,
                    help="Angle index to process (In case of PSOCT data type)")
+    p.add_argument('--return_complex', type = bool, default = False,
+                   help="Return Complex64 or Float32 data type (In case of PSOCT data type)")
     return p
 
 
@@ -54,101 +56,31 @@ def process_tile(params: dict):
     crop = params["crop"]
     tile_size = params["tile_size"]
     mosaic = params["mosaic"]
-
+    data_type = params["data_type"]
+    pol_index = params["pol_index"]
+    return_complex = params["return_complex"]
     # Load the tile
-    oct = OCT(f)
-    vol = oct.load_image(crop=crop)
-    vol = preprocess_volume(vol)
-
+    if data_type == 'OCT':
+        oct = OCT(f)
+        vol = oct.load_image(crop=crop)
+        vol = preprocess_volume(vol)
+    elif data_type == 'PSOCT':
+        oct = ThorOCT(f)
+        if pol_index == 0:
+            oct.load(erase_polarization_2=True, return_complex = return_complex)
+            vol = oct.polarization1
+        else:
+            oct.load(erase_polarization_1=True, return_complex = return_complex)
+            vol = oct.polarization2
+        vol = ThorOCT.preprocess_volume_PSOCT(vol)
     # Rescale the volume
     vol = resize(vol, tile_size, anti_aliasing=True, order=1, preserve_range=True)
-
     # Compute the tile position
     rmin = mx * vol.shape[1]
     cmin = my * vol.shape[2]
     rmax = rmin + vol.shape[1]
     cmax = cmin + vol.shape[2]
     mosaic[:, rmin:rmax, cmin:cmax] = vol
-
-def extract_positions_from_scan(scan_file_path: str):
-    """
-    Extracts the x, y positions from the .scan file.
-
-    Parameters:
-    - scan_file_path: Path to the .scan file.
-
-    Returns:
-    - A list of tuples containing the x, y positions.
-    """
-    positions = []
-    with open(scan_file_path, 'r') as file:
-        lines = file.readlines()
-
-        # Find the start of the positions section
-        positions_section = False
-        for line in lines:
-            line = line.strip()
-
-            # Mark the start of the positions section
-            if line == "------Positions------":
-                positions_section = True
-                continue
-
-            # If in the positions section, extract x, y values
-            if positions_section:
-                if line:  # Ignore empty lines
-                    # Split by comma and convert to float
-                    x, y = map(float, line.split(','))
-                    positions.append((x, y, 0))
-
-    return positions
-
-def get_PSOCT_tiles_ids(tiles_directory: str, number_of_angles:int = 2):
-    """
-    Get the .scan file and all .oct files from the tiles_directory.
-
-    Parameters:
-    - tiles_directory: Path to the directory containing the OCT tiles.
-    - number_of_angles: Number of acquisition angles.
-
-    Returns:
-    - positions: positions of the tiles in 3d
-    - grouped_files: list of file paths ordered by angles.
-    """
-    # Convert the tiles_directory to a Path object
-    tiles_path = Path(tiles_directory)
-    
-    if not tiles_path.is_dir():
-        raise ValueError(f"Provided path '{tiles_directory}' is not a valid directory.")
-    
-    # Initialize variables to store the results
-    scan_file = None
-    oct_files = []
-    grouped_files = [[] for _ in range(number_of_angles)]
-
-    # Iterate through files in the directory
-    for file in tiles_path.iterdir():
-        # Check for .scan file
-        if file.suffix == ".scan":
-            scan_file = file
-            positions = extract_positions_from_scan(scan_file)
-        # Collect .oct files
-        elif file.suffix == ".oct":
-            oct_files.append(file)
-    
-    # If no .scan file is found, raise a warning
-    if scan_file is None:
-        print("Warning: No .scan file found in the directory.")
-    
-    # If no .oct files are found, raise a warning
-    if not oct_files:
-        print("Warning: No .oct files found in the directory.")
-        
-    for i, oct_file in enumerate(oct_files):
-        angle_index = i % number_of_angles  # Determine the angle based on file index
-        grouped_files[angle_index].append(oct_file)
-    
-    return grouped_files, positions
 
 def main():
     # Parse arguments
@@ -165,12 +97,13 @@ def main():
     data_type = args.data_type
     pol_index = args.polarization
     angle_index = args.angle_index
+    return_complex = args.return_complex
 
     # Analyze the tiles
     if data_type == 'OCT':
         tiles, tiles_pos = reconstruction.get_tiles_ids(tiles_directory, z=z)
     elif data_type == 'PSOCT':
-        tiles, tiles_pos = get_PSOCT_tiles_ids(tiles_directory)
+        tiles, tiles_pos = ThorOCT.get_PSOCT_tiles_ids(tiles_directory)
         tiles = tiles[angle_index]
     mx = [tiles_pos[i][0] for i in range(len(tiles_pos))]
     my = [tiles_pos[i][1] for i in range(len(tiles_pos))]
@@ -187,29 +120,35 @@ def main():
         vol = oct.load_image(crop=crop)
         vol = preprocess_volume(vol)
         resolution = [oct.resolution[2], oct.resolution[0], oct.resolution[1]]
-    elif data_type == 'PSCOT':
+    elif data_type == 'PSOCT':
         oct = ThorOCT(tiles[0])
         if pol_index == 0:
-            thor = ThorOCT.load(erase_polarization_2=True)
-            vol = thor.polarization1
+            oct.load(erase_polarization_2=True, return_complex = return_complex)
+            vol = oct.polarization1
         else:
-            vol = ThorOCT.load(erase_polarization_1=True)
-            vol = thor.polarization2
-        vol = preprocess_volume(vol)
-        resolution = [15, 15, 15] # not sure of these values
+            oct.load(erase_polarization_1=True, return_complex = return_complex)
+            vol = oct.polarization2
+        vol = ThorOCT.preprocess_volume_PSOCT(vol)
+        resolution = [oct.resolution[2], oct.resolution[0], oct.resolution[1]]
+        print(f"Resolutoin: z = {resolution[0]} , x = {resolution[1]} , y = {resolution[2]} ") 
     
 
     # Compute the rescaled tile size based on the minimum target output resolution
     if output_resolution == -1:
         tile_size = vol.shape
     else:
-        tile_size = [int(vol.shape[i] * resolution[i] * 1000 / output_resolution) for i in range(3)]
+        tile_size = [int(vol.shape[i] * resolution[i] * 1000 / output_resolution) for i in range(3)]     
     mosaic_shape = [tile_size[0], n_mx * tile_size[1], n_my * tile_size[2]]
-
+    print("Tile size:", tile_size)
+    
     # Create the zarr persistent array
     process_sync_file = str(zarr_file).replace(".zarr", ".sync")
     synchronizer = zarr.ProcessSynchronizer(process_sync_file)
-    mosaic = zarr.open(zarr_file, mode="w", shape=mosaic_shape, dtype=np.float32, chunks=tile_size,
+    if return_complex == True:
+        mosaic = zarr.open(zarr_file, mode="w", shape=mosaic_shape, dtype=np.complex64, chunks=tile_size,
+                       synchronizer=synchronizer)
+    else:
+        mosaic = zarr.open(zarr_file, mode="w", shape=mosaic_shape, dtype=np.float32, chunks=tile_size,
                        synchronizer=synchronizer)
 
     # Create a params dictionary for every tile
@@ -220,7 +159,10 @@ def main():
             "tile_pos": tiles_pos[i],
             "crop": crop,
             "tile_size": tile_size,
-            "mosaic": mosaic
+            "mosaic": mosaic,
+            "data_type": data_type,
+            "pol_index": pol_index,
+            "return_complex": return_complex
         })
 
     # Process the tiles in parallel
@@ -234,10 +176,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # Example usage
-    # scan_file, oct_files = get_PSOCT_tiles_ids("C:/Users/Mohamad Hawchar/Concordia University - Canada/NeuralABC as-psOCT Samples - data/2024_07_25_mouse_CB_1slice_2anglesliceIdx1_SUCCESS")
-    # print(f"Scan file: {len(scan_file)}")
-    # print(f"Angles: {len(oct_files)}")
-    # print(f"Angle 1 length: {len(oct_files[0])}")
-    # print(f"Angle 1 length: {len(oct_files[1])}")
-
