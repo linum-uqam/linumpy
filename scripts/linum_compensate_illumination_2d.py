41a464a7efbd9acda56b9ba5560b91921bf546a4
Joël Lefebvre <lefebvre.joel@uqam.ca>
2024-12-17 11:19:01 -0500

    Removing old scripts and renaming a few to make things clearer (issue #30)

--- scripts/linum_compensate_illumination_2d.py
+++ scripts/linum_compensate_illumination_2d.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Uses the BaSiC algorithm to estimate and compensate illumination inhomogeneities in a mosaic grid"""
+"""Uses the BaSiC algorithm to estimate and compensate illumination inhomogeneities in a 2D mosaic grid"""
 
 import argparse
 from pathlib import Path
