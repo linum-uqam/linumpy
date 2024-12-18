e5d204eeb0d771c500684b7785c5d809b6aba436
CHrlS98 <charles.poirier@usherbrooke.ca>
2024-12-18 14:45:49 -0500

    Optional multiprocessing

--- linumpy/utils/io.py
+++ linumpy/utils/io.py
@@ -14,6 +14,7 @@ def parse_processes_arg(n_processes):
 
 
 def add_processes_arg(parser):
-    a = parser.add_argument('--n_processes', type=int,
-                            help=f'Number of processes to use [{DEFAULT_N_CPUS}].')
+    a = parser.add_argument('--n_processes', type=int, default=1,
+                            help='Number of processes to use. -1 to use \n'
+                                 'all cores [%(default)s].')
     return a
