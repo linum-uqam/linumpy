ba76782116b40fe0993a516e08da63cac2cc0750
CHrlS98 <charles.poirier@usherbrooke.ca>
2025-06-03 17:24:37 -0400

    More test data

--- linumpy/io/test_data.py
+++ linumpy/io/test_data.py
@@ -1,15 +1,16 @@
 # -*- coding:utf-8 -*-
 from linumpy import LINUMPY_HOME
 from linumpy.io.zarr import save_omezarr
+from skimage.data import cells3d
 import os
-import logging
 import numpy as np
 import dask.array as da
 
 
 def get_data(name):
     data = {
-        'mosaic_3d_omezarr': _get_mosaic_3d_omezarr()
+        'mosaic_3d_omezarr': _get_mosaic_3d_omezarr(),
+        'raw_tiles': _get_raw_tiles()
     }
     if name not in data.keys():
         raise ValueError(f'Unknown key for data: {name}')
@@ -32,3 +33,59 @@ def _get_mosaic_3d_omezarr():
                      n_levels=5, overwrite=False)
 
     return filename
+
+
+def _get_scan_info(nx, ny, top_z, bottom_z, width_mm,
+                   height_mm, x_pos_mm, y_pos_mm, z_pos_mm):
+    focus_z = int((top_z + bottom_z) / 2)
+    scan_info = \
+f"""Scan info
+nx: {nx}
+ny: {ny}
+n_repeat: 1
+width: {width_mm}
+height: {height_mm}
+n_extra: 0
+line_rate: 80
+exposure: 23
+alinerepeat: 1
+top_z: {top_z}
+bottom_z: {bottom_z}
+focus_z: {focus_z}
+stage_x_pos_mm: {x_pos_mm}
+stage_y_pos_mm: {y_pos_mm}
+stage_z_pos_mm: {z_pos_mm}
+"""
+    return scan_info
+
+
+def _get_raw_tiles():
+    _create_linumpy_home_if_not_exists()
+    folder = os.path.join(LINUMPY_HOME, 'raw_tiles')
+    bounds_xy = [(0, 140), ((256-140), 256)]
+    bounds_z = [(0, 35), ((60-35), 60)]
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+        data = np.mean(cells3d(), axis=1)  # (order z, y, x)
+        for zi, (zmin, zmax) in enumerate(bounds_z):
+            for yi, (ymin, ymax) in enumerate(bounds_xy):
+                for xi, (xmin, xmax) in enumerate(bounds_xy):
+                    tile_folder = os.path.join(folder, f'tile_x0{xi}_y0{yi}_z0{zi}')
+                    os.makedirs(tile_folder)
+                    tile_xyz = data[zmin:zmax + 1, ymin:ymax, xmin:xmax]
+                    tile_xyz = tile_xyz[:, ::-1, ::-1]
+                    tile_xyz[:, 0, 0] = 2.0*np.max(tile_xyz)
+                    tile_xyz.astype(np.float32).reshape(-1, order='F').tofile(
+                        os.path.join(tile_folder, 'image_00000.bin'))
+                    nx = width = xmax - xmin
+                    ny = height = ymax - ymin
+                    top_z = zmin
+                    bottom_z = zmax
+                    stage_x_pos = xmin
+                    stage_y_pos = ymin
+                    stage_z_pos = zmin
+                    info = _get_scan_info(nx, ny, top_z, bottom_z, width, height,
+                                          stage_x_pos, stage_y_pos, stage_z_pos)
+                    with open(os.path.join(tile_folder, 'info.txt'), 'w') as f:
+                        f.writelines(info)
+    return folder
--- scripts/tests/test_create_mosaic_grid_3d.py
+++ scripts/tests/test_create_mosaic_grid_3d.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+from linumpy.io.test_data import get_data
 
 
 def test_help(script_runner):
     ret = script_runner.run(['linum_create_mosaic_grid_3d.py', '--help'])
+    assert ret.success
+
+
+def test_execution_from_directory(script_runner, tmp_path):
+    input = get_data('raw_tiles')
+    output = tmp_path / 'output.ome.zarr'
+    ret = script_runner.run(['linum_create_mosaic_grid_3d.py', output,
+                             '--from_root_directory', input, '-z', 0, '-r', 2])
     assert ret.success
