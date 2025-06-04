438e46d9390386bea599a13a540d0b32f47c7938
CHrlS98 <charles.poirier@usherbrooke.ca>
2025-06-04 15:54:18 -0400

    Add mosaics_grid tests

--- scripts/tests/test_create_mosaic_grid.py
+++ scripts/tests/test_create_mosaic_grid.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+from linumpy.io.test_data import get_data
 
 
 def test_help(script_runner):
     ret = script_runner.run(['linum_create_mosaic_grid.py', '--help'])
+    assert ret.success
+
+
+def test_execution(script_runner, tmp_path):
+    input = get_data('raw_tiles')
+    output = tmp_path / 'output.zarr'
+
+    ret = script_runner.run(['linum_create_mosaic_grid.py',
+                             input, output, '--n_cpus', 1])
     assert ret.success
--- scripts/tests/test_create_mosaic_grid_3d.py
+++ scripts/tests/test_create_mosaic_grid_3d.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 from linumpy.io.test_data import get_data
+import os
 
 
 def test_help(script_runner):
@@ -14,3 +15,15 @@ def test_execution_from_directory(script_runner, tmp_path):
     ret = script_runner.run(['linum_create_mosaic_grid_3d.py', output,
                              '--from_root_directory', input, '-z', 0, '-r', 2])
     assert ret.success
+
+
+def test_execution_from_list(script_runner, tmp_path):
+    input = get_data('raw_tiles')
+    output = tmp_path / 'output.ome.zarr'
+    ret = script_runner.run(['linum_create_mosaic_grid_3d.py', output,
+                             '--from_tiles_list',
+                             os.path.join(input, 'tile_x00_y00_z00'),
+                             os.path.join(input, 'tile_x01_y00_z00'),
+                             os.path.join(input, 'tile_x00_y01_z00'),
+                             os.path.join(input, 'tile_x01_y01_z00'),
+                             '-r', 2])
