608ee87937924124b7fa56fbbbf987f152a17921
CHrlS98 <charles.poirier@usherbrooke.ca>
2025-06-23 11:30:08 -0400

    Rename psf scripts

--- scripts/tests/test_compensate_psf_model_free.py
+++ scripts/tests/test_compensate_psf_model_free.py
@@ -3,5 +3,5 @@
 
 
 def test_help(script_runner):
-    ret = script_runner.run(['linum_axial_psf_correction.py', '--help'])
+    ret = script_runner.run(['linum_compensate_psf_model_free.py', '--help'])
     assert ret.success
