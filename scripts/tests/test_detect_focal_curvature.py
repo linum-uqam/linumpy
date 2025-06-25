04dccc11fcc66e7dc8bf830f4d46fae46da230da
CHrlS98 <charles.poirier@usherbrooke.ca>
2025-06-25 14:18:37 -0400

    Tests and bug fix

--- scripts/tests/test_detect_focal_curvature.py
+++ scripts/tests/test_detect_focal_curvature.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+from linumpy.io.test_data import get_data
 
 
 def test_help(script_runner):
     ret = script_runner.run(['linum_detect_focal_curvature.py', '--help'])
+    assert ret.success
+
+
+def test_execute(script_runner, tmp_path):
+    input = get_data('mosaic_3d_omezarr')
+    output = tmp_path / 'fix_focal.ome.zarr'
+    ret = script_runner.run(['linum_detect_focal_curvature.py', input, output])
     assert ret.success
--- scripts/tests/test_estimate_transform.py
+++ scripts/tests/test_estimate_transform.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+from linumpy.io.test_data import get_data
 
 
 def test_help(script_runner):
     ret = script_runner.run(['linum_estimate_transform.py', '--help'])
+    assert ret.success
+
+
+def test_execute(script_runner, tmp_path):
+    input = get_data('aip')
+    output = tmp_path / 'transform.npy'
+    ret = script_runner.run(['linum_estimate_transform.py', input, output])
     assert ret.success
