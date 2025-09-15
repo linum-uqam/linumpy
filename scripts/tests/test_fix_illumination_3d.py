1c6396fd8908b7e9024251b3df443883b3153274
CHrlS98 <charles.poirier@usherbrooke.ca>
2025-09-15 12:13:36 -0400

    lower max iterations to 10

--- scripts/tests/test_fix_illumination_3d.py
+++ scripts/tests/test_fix_illumination_3d.py
@@ -11,5 +11,6 @@ def test_help(script_runner):
 def test_execute(script_runner, tmp_path):
     input = get_data('mosaic_3d_omezarr')
     output = tmp_path / 'fix_illumination.ome.zarr'
-    ret = script_runner.run(['linum_fix_illumination_3d.py', input, output, '--max_iterations', 50])
+    ret = script_runner.run(['linum_fix_illumination_3d.py', input, output,
+                             '--max_iterations', 10])
     assert ret.success
