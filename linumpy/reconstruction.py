6f2c3d7088e00d99ee8be597a943560cea0c6bac
Frans Irgolitsch <frans.irgolitsch@polymtl.ca>
2025-09-18 15:03:13 -0400

    Merged filters into a single list comprehension

--- linumpy/reconstruction.py
+++ linumpy/reconstruction.py
@@ -50,11 +50,8 @@ def get_tiles_ids(directory, z: int = None):
     else:
         tiles_to_process = f"tile_*"
     tiles = list(input_directory.rglob(tiles_to_process))
-    tiles = [t for t in tiles if t.name.startswith('tile_')]
-    # Filter out all files
-    tiles = [t for t in tiles if not os.path.isfile(t)]
+    tiles = [t for t in tiles if t.name.startswith('tile_') and not os.path.isfile(t)]
     tile_ids = get_tiles_ids_from_list(tiles)
-
     return tiles, tile_ids
 
 
