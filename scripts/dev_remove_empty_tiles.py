9ce6c9eca69babcbb3e704ddd6662a85de397663
Joël Lefebvre <lefebvre.joel@uqam.ca>
2025-10-15 10:58:25 -0400

    Added loop for easier cleanup

--- scripts/dev_remove_empty_tiles.py
+++ scripts/dev_remove_empty_tiles.py
@@ -14,10 +14,6 @@ from tqdm import tqdm
 from linumpy import reconstruction
 
 # Parameters
-z = 31
-directory = Path(f"E:\Frans\sub-17-reembedded\slice_z{z:02d}")
-tiles_directory = directory / "tiles"
-figure_file = directory / "tile_cleaning.png"
 tiles_margin = 1
 saturation = 99.7
 median_size = 5
@@ -32,75 +28,84 @@ def getLargestCC(segmentation):
     return largestCC
 
 
-# Get a quick reconstruction (without overlap)
-mosaic = reconstruction.quick_stitch(tiles_directory, z=z, overlap_fraction=0.0, n_rot=0, flip_lr=False, flip_ud=False)
+z_start = 5
+z_end = 84
 
-# Normalize the image
-mosaic = (mosaic.astype(np.float32) - mosaic.min()) / (np.percentile(mosaic, saturation) - mosaic.min())
-mosaic[mosaic > 1] = 1
+for i in range(z_start, z_end):
+    z = i
+    directory = Path(f"D:\sub-21\slice_z{z:02d}")
+    tiles_directory = directory / "tiles"
+    figure_file = directory / "tile_cleaning.png"
 
-# Rescale
-mosaic = rescale(mosaic, 1 / scaling_factor, anti_aliasing=True)
+    # Get a quick reconstruction (without overlap)
+    mosaic = reconstruction.quick_stitch(tiles_directory, z=z, overlap_fraction=0.0, n_rot=0, flip_lr=False, flip_ud=False)
 
-# Get the mosaic shape
-mosaic_info = reconstruction.get_mosaic_info(tiles_directory, z=z, overlap_fraction=0.0, use_stage_positions=False)
+    # Normalize the image
+    mosaic = (mosaic.astype(np.float32) - mosaic.min()) / (np.percentile(mosaic, saturation) - mosaic.min())
+    mosaic[mosaic > 1] = 1
 
-# Mask the tissue
-threshold = threshold_li(mosaic)
-mask = mosaic > threshold
-mask = median_filter(mask, median_size)
+    # Rescale
+    mosaic = rescale(mosaic, 1 / scaling_factor, anti_aliasing=True)
 
-# Detect the empty tiles
-tiles_grid_mask = np.zeros(mosaic_info['mosaic_grid_shape'], dtype=bool)
-for p, p_px in zip(mosaic_info['mosaic_tile_pos'], mosaic_info['tiles_pos_px']):
-    rmin = p_px[0] // scaling_factor
-    cmin = p_px[1] // scaling_factor
-    rmax = rmin + mosaic_info['tile_shape_px'][0] // scaling_factor
-    cmax = cmin + mosaic_info['tile_shape_px'][1] // scaling_factor
-    foo = mask[rmin:rmax, cmin:cmax]
-    if not np.any(foo):
-        tiles_grid_mask[p[0], p[1]] = True
+    # Get the mosaic shape
+    mosaic_info = reconstruction.get_mosaic_info(tiles_directory, z=z, overlap_fraction=0.0, use_stage_positions=False)
 
-# Fill holes
-tiles_grid_mask = ~binary_fill_holes(~tiles_grid_mask)
+    # Mask the tissue
+    threshold = threshold_li(mosaic)
+    mask = mosaic > threshold
+    mask = median_filter(mask, median_size)
 
-# Keep the largest CC
-if keep_largest_island:
-    tiles_grid_mask = ~getLargestCC(~tiles_grid_mask)
+    # Detect the empty tiles
+    tiles_grid_mask = np.zeros(mosaic_info['mosaic_grid_shape'], dtype=bool)
+    for p, p_px in zip(mosaic_info['mosaic_tile_pos'], mosaic_info['tiles_pos_px']):
+        rmin = p_px[0] // scaling_factor
+        cmin = p_px[1] // scaling_factor
+        rmax = rmin + mosaic_info['tile_shape_px'][0] // scaling_factor
+        cmax = cmin + mosaic_info['tile_shape_px'][1] // scaling_factor
+        foo = mask[rmin:rmax, cmin:cmax]
+        if not np.any(foo):
+            tiles_grid_mask[p[0], p[1]] = True
 
-# Add a margin
-tiles_grid_mask = binary_erosion(tiles_grid_mask, disk(tiles_margin), border_value=1)
+    # Fill holes
+    tiles_grid_mask = ~binary_fill_holes(~tiles_grid_mask)
 
-# Convert to a list of tiles to remove
-tiles_to_delete = []
-for i in range(len(mosaic_info["tiles"])):
-    p = mosaic_info["mosaic_tile_pos"][i]
-    if tiles_grid_mask[p[0], p[1]]:
-        tiles_to_delete.append(mosaic_info["tiles"][i])
+    # Keep the largest CC
+    if keep_largest_island:
+        tiles_grid_mask = ~getLargestCC(~tiles_grid_mask)
 
-# Display the tiles that will be removed
-overlay = label2rgb(resize(tiles_grid_mask, mosaic.shape), mosaic, alpha=0.1)
-plt.imshow(overlay)
-plt.title(f"Slice z={z}")
-plt.savefig(fname=figure_file, dpi=300, bbox_inches='tight', pad_inches=0)
-plt.show()
+    # Add a margin
+    tiles_grid_mask = binary_erosion(tiles_grid_mask, disk(tiles_margin), border_value=1)
 
-# Wait for user ok
-question = f"Do you wish to delete the red tiles in slice z={z} (y/n)? : "
-delete_tiles = None
-while delete_tiles is None:
-    answer = input(question)
-    if answer.lower() == "y":
-        delete_tiles = True
-    elif answer.lower() == "n":
-        delete_tiles = False
-    else:
-        print("Wrong input! Accepted values are: y or n")
+    # Convert to a list of tiles to remove
+    tiles_to_delete = []
+    for i in range(len(mosaic_info["tiles"])):
+        p = mosaic_info["mosaic_tile_pos"][i]
+        if tiles_grid_mask[p[0], p[1]]:
+            tiles_to_delete.append(mosaic_info["tiles"][i])
 
-# Remove the selected background tiles for this slice
-if delete_tiles:
-    # TODO: save the tile deletion map for reference
+    # Display the tiles that will be removed
+    overlay = label2rgb(resize(tiles_grid_mask, mosaic.shape), mosaic, alpha=0.1)
+    plt.imshow(overlay)
+    plt.title(f"Slice z={z}")
+    plt.savefig(fname=figure_file, dpi=300, bbox_inches='tight', pad_inches=0)
+    plt.show()
 
-    # Loop over the tiles to delete and remove their data directory
-    for t in tqdm(tiles_to_delete, desc="Deleting tiles"):
-        shutil.rmtree(t)
+    # Wait for user ok
+    question = f"Do you wish to delete the red tiles in slice z={z} (y/n)? : "
+    delete_tiles = None
+    while delete_tiles is None:
+        answer = input(question)
+        if answer.lower() == "y":
+            delete_tiles = True
+        elif answer.lower() == "n":
+            delete_tiles = False
+        else:
+            print("Wrong input! Accepted values are: y or n")
+
+    # Remove the selected background tiles for this slice
+    if delete_tiles:
+        # TODO: save the tile deletion map for reference
+
+        # Loop over the tiles to delete and remove their data directory
+        for t in tqdm(tiles_to_delete, desc="Deleting tiles"):
+            shutil.rmtree(t)
